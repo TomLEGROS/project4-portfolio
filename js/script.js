4ab1472538ffd6fd0fed3989a7f549bff043feb6
unknown <tlegros@gamingcampus.local>
2021-11-17 11:36:16 +0100

    initial commit + manual merge

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+
+
+$(".dropdown-trigger").dropdown();
+
+$(document).ready(function(){
+  $('.modal').modal();
+});
+
+$('.carousel').carousel();
+setInterval(function() {
+  $('.carousel').carousel('next');
+}, 5000);
+
+$(document).ready(function(){
     $('.parallax').parallax();
   });
