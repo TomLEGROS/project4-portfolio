c7b7c610276ff52837fc00fee98a4215471e8a7a
unknown <hcaroumbin@gamingcampus.local>
2021-11-17 11:32:08 +0100

    merge

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
-
-$(".dropdown-trigger").dropdown();
-
-$(document).ready(function(){
-  $('.modal').modal();
-});
-
-$('.carousel').carousel();
-setInterval(function() {
-  $('.carousel').carousel('next');
-}, 5000);
+    $('.parallax').parallax();
+  });
