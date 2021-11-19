5eceda9e782539c00896f1f26556a25ec4f3cdc8
unknown <tlegros@gamingcampus.local>
2021-11-19 15:45:39 +0100

    easter

--- js/script.js
+++ js/script.js
@@ -21,3 +21,19 @@ $(document).ready(function(){
   $(document).ready(function(){
   $('.collapsible').collapsible();
 });
+
+var click = 0;
+var yamete = new Audio('audio/yamete.mp3');
+
+function easteregg(){
+  document.getElementById("neko").classList.add('hide');
+  click += 1;
+  document.getElementById('a').innerHTML="click : " + click;
+  if (click == 10){
+    document.getElementById("neko").classList.remove('hide');
+    yamete.play();
+    click = 0
+
+
+  }
+}
